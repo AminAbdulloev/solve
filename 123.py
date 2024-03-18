b7b19670f2637265ffeb83b80cccfed8da778357
AminAbdulloev <amin.abdulloevvv005@bk.ru>
2024-03-18 22:44:22 +0800

    rrr

--- 123.py
+++ 123.py
@@ -1,17 +1,1 @@
-import cmath
-
-def solve_incomplete_quadratic_equation(c):
-    x1 = cmath.sqrt(-c)
-    x2 = -cmath.sqrt(-c)
-    return x1, x2
-
-# Ввод свободного члена уравнения
-c = float(input("Введите свободный член c: "))
-
-# Решение уравнения
-roots = solve_incomplete_quadratic_equation(c)
-
-# Вывод корней уравнения
-print("Корни уравнения:")
-for i, root in enumerate(roots):
-    print(f"x{i+1} =", root)
+print("Hello world")
